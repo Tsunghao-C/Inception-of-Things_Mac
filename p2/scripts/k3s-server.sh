c2471bccd10dd4f10ba64f5d3964f7f09cf721da
Tsunghao <tsunghao.chen12@gmail.com>
2025-03-21 00:03:34 +0100

    part 2 works on mac as well

--- p2/scripts/k3s-server.sh
+++ p2/scripts/k3s-server.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
 
+apt update -y
+
 # Install K3s
 echo "[Step 1] Installing K3s..."
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1" sh -s -
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_EXEC="server --node-external-ip=$1 --bind-address=$1"
+sudo curl -sfL https://get.k3s.io | sh -
+if [ $? -ne 0 ]; then
+    echo "Failed to install K3s. Exiting."
+    exit 1
+fi
+
 sleep 10
+
+# alias k for kubectl
+echo 'export PATH="/sbin:$PATH"' >> $HOME/.bashrc
+echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh > /dev/null
 
 # Deploy Applications
 echo "[Step 2] Deploying Applications"
-kubectl apply -f /vagrant/confs/services.yaml
-sleep 7
-kubectl apply -f /vagrant/confs/deployments.yaml
-sleep 7
-kubectl apply -f /vagrant/confs/ingress.yaml
-sleep 7
 
-echo "[Step 3] Checking Deployments...."
+echo "[App1] Deploying"
+kubectl create configmap app-one-html --from-file /vagrant/app1/index.html
+kubectl apply -f /vagrant/app1/deployment.yaml
+kubectl apply -f /vagrant/app1/service.yaml
+echo "[App1] Done"
+
+echo "[App2] Deploying"
+kubectl create configmap app-two-html --from-file /vagrant/app2/index.html
+kubectl apply -f /vagrant/app2/deployment.yaml
+kubectl apply -f /vagrant/app2/service.yaml
+echo "[App2] Done"
+
+echo "[App3] Deploying"
+kubectl create configmap app-three-html --from-file /vagrant/app3/index.html
+kubectl apply -f /vagrant/app3/deployment.yaml
+kubectl apply -f /vagrant/app3/service.yaml
+echo "[App3] Done"
+
+echo "[Ingress] Deploying"
+kubectl apply -f /vagrant/ingress.yaml
+echo "[Ingress] Done"
+
+sleep 10
+
+# Check deployments
+echo "Checking Deployments...."
 kubectl get deployments
-
-echo "[Step 4] Checking Ingress...."
+echo "Checking Ingress...."
 kubectl get ingress
