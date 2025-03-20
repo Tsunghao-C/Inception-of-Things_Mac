395433a6942d0f3563c6742d2fe48c2280c5da45
Tsunghao <tsunghao.chen12@gmail.com>
2025-03-20 23:47:22 +0100

    update the same as linux version

--- p1/scripts/k3s-server.sh
+++ p1/scripts/k3s-server.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1" sh -s -
-sleep 15
+sudo apt update -y
+
+# Set up env for K3s
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_EXEC="server --node-ip $1 --bind-address=$1"
+# Install K3s
+echo "[LOG] Installing K3s"
+sudo curl -sfL https://get.k3s.io | sh -
+if [ $? -ne 0 ]; then
+    echo "Failed to install K3s. Exiting."
+    exit 1
+fi
+echo "[LOG] Copy token"
+TIMEOUT=30
+while [ ! -f  /var/lib/rancher/k3s/server/node-token ]; do
+    sleep 1
+    TIMEOUT=$((TIMEOUT - 1))
+    if [ "$TIMEOUT" -eq 0 ]; then
+        echo "Token file failed to generate."
+        exit 1
+    fi
+done
 cp /var/lib/rancher/k3s/server/node-token /vagrant/.
+
+# alias k for kubectl
+echo 'export PATH="/sbin:$PATH"' >> $HOME/.bashrc
+echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh > /dev/null
--- p1/scripts/k3s-worker.sh
+++ p1/scripts/k3s-worker.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /vagrant/node-token --node-ip=$2" sh -s -
+sudo apt update -y
+
+# Set up env for K3s
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /vagrant/node-token --node-ip=$2"
+sudo curl -sfL https://get.k3s.io | sh -
+if [ $? -ne 0 ]; then
+    echo "Failed to install K3s. Exiting."
+    exit 1
+fi
+
+# alias k for kubectl
+echo 'export PATH="/sbin:$PATH"' >> $HOME/.bashrc
+echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh > /dev/null
